3c5f85d04713e4a8a1953ef776c1248e9adb8475
Mike Skalandunas <mskalandunas@gmail.com>
2016-09-29 15:52:02 -0700

    add new server

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('port', (process.env.PORT || 3000));
-
-app.use(express.static(__dirname + '/app/'));
-
-app.get('/*', function(req, res) {
-  res.status(404).sendFile(__dirname + '/app/index.html');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Server up and running at localhost:' + app.get('port'));
+app.use(express.static(__dirname + '/app/'))
+app.listen(port, () => {
+  console.log('Listening.');
 });

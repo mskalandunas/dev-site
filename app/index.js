d9905baabb588769fae4f30f01787ec816288c3d
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-04 00:08:52 -0700

    start overhaul

---
+++ app/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import './css/normalize.css'
+import './css/skeleton.css'
+import './css/additional.css'
+import './css/main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+
+import './js/components/landing'

fb7a21f25038c68060e7491be4c9417cf725dd11
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-14 00:05:56 -0800

    new direction

--- app/index.js
+++ app/index.js
@@ -2,11 +2,8 @@
 
 import './css/normalize.css'
 import './css/skeleton.css'
-import './css/additional.css'
-import './css/main.css'
+import './css/style.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
-
-import './js/components/landing'

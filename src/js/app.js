e268c9aea4d1589e933da907e991ed1e31d2dab6
Mike Skalandunas <mskalandunas@gmail.com>
2017-01-25 23:38:57 -0800

    add logic for just 1 project at a time

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,31 @@
 'use strict';
 
 const projects = [...document.querySelectorAll(`a[data-project]`)];
+const active   = {
+  'redirect'     : false,
+  'architect'    : false,
+  'mosaic-audio' : false,
+  'mosaicmusic'  : false
+};
 
 function toggleActive(e) {
   e.preventDefault();
-  let project = document.querySelector(`div[data-description=${this.dataset.project}]`);
+  let project = this.dataset.project;
+  let current = document.querySelector(`div[data-description=${project}]`);
 
+  for (let i in active) {
+    if (active[i] && i !== project) {
+      let a = document.querySelector(`a[data-project="${i}"]`);
+      let b = document.querySelector(`div[data-description=${i}]`);
+      a.classList.toggle('active');
+      b.classList.toggle('hidden');
+      active[i] = false;
+    };
+  };
+
+  active[project] = true;
   this.classList.toggle('active');
-  project.classList.toggle('hidden');
+  current.classList.toggle('hidden');
 };
 
 projects.forEach(project => project.addEventListener('click', toggleActive));
